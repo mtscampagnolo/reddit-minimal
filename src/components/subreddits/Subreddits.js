57542fc288d260f354beea13685066f35c4731ae
Mateus Campagnolo <mtscampagnolo@gmail.com>
2024-06-27 15:32:39 +0100

    Styling subreddits nav

--- src/components/subreddits/Subreddits.js
+++ src/components/subreddits/Subreddits.js
@@ -2,6 +2,7 @@ import React, {useEffect} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import {loadSubreddits, selectSubreddits, subredditsIsLoading, subredditsHasError} from './subredditsSlice';
 import {getPostBySubreddit} from '../postList/postListSlice';
+import styles from './subreddits.module.css';
 
 const Subreddits = () => {
   const dispatch = useDispatch();
@@ -26,7 +27,7 @@ const Subreddits = () => {
       <h2>Subreddits</h2>
       <ul>
         {subredditList.map((item, key) => {
-          return <li key={key} onClick={() => dispatch(getPostBySubreddit(item))} >{item}</li>
+          return <li className={styles.subreddit} key={key} onClick={() => dispatch(getPostBySubreddit(item))} >{item}</li>
         })}
       </ul>
     </div>
